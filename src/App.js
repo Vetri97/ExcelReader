9ab5251a455c975dbf0ffdca034948c56fffb1b6
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2023-05-23 12:45:17 +0530

    API integration, sending file as form data and other UI  functionalities
    API integration, sending file as form data and other UI  functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+
+    if (
+      event.target.files[0].type === "application/vnd.ms-excel" ||
+      event.target.files[0].type ===
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ) {
+      setTimeout(() => {
+        apicall();
+      }, 1500);
+    }
+  };
+
+  const apicall = () => {
+    const data = new FormData();
+    data.append("file", selectedFile);
+    console.log(data);
+    axios
+      .post("http://localhost:8000/upload", data)
+      .then((response) => response.data)
+      .then(
+        (result) => {
+          document.getElementById("uploadFile").value = "";
+        },
+        (err) => {
+          document.getElementById("uploadFile").value = "";
+        }
+      );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        id="uploadFile"
+        single
+        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+        onChange={changeHandler}
+      ></input>
     </div>
   );
-}
+};
 
 export default App;
